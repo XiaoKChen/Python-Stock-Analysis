217bcf26bab4079edf6b80d2182f7aa2f6a2e578
Xiaokang Chen <Xiaokang.chen888@gmail.com>
2023-03-21 23:04:27 -1000

    Added def output_to_csv

--- src/stockmarket_trend.py
+++ src/stockmarket_trend.py
@@ -6,22 +6,24 @@ import time
 from dotenv import load_dotenv
 load_dotenv()
 
-ticker = "AAPL"
-api_key = os.environ['APIKEY']
-
 def get_stock_price():
     url = f"https://api.twelvedata.com/price?symbol={ticker}&apikey={api_key}"
     response = requests.get(url).json()
     price = response['price'][:-3]
     print(f'{time.ctime(time.time())} : {price}')
+    return (time.ctime(time.time()), price)
 
-# def output_to_csv(time, price):
-    
+def output_to_csv(input_time, input_price):
+    writer.writerow({input_time, input_price})
 
+ticker    = 'AAPL'
+api_key   = os.environ['APIKEY']
 starttime = time.time()
-
-csv_file = open(f'../{time.ctime(time.time())}')
+csv_file  = open(f'../csv_data/{time.ctime(starttime)}.csv', 'w')
+writer = csv.writer(csv_file)
+writer.writerow({'Time', 'Price'})
 
 while True:
-    get_stock_price()
+    input_time, input_price = get_stock_price()
+    output_to_csv(input_time, input_price)
     time.sleep(5.0 - ((time.time() - starttime) % 5.0))
