784e2994c02a9cf3ebb6d02a74773b25df0f81ef
ZiHeYan03 <ziheyan9@gmail.com>
2023-03-21 17:22:40 -1000

    Update

--- src/stockmarket_trend.py
+++ src/stockmarket_trend.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import requests
 import time
 from dotenv import load_dotenv
@@ -7,12 +8,13 @@ load_dotenv()
 ticker = "AAPL"
 api_key = os.environ['APIKEY']
 
-def get_stock_price(import_ticker, import_api_key):
-    url = f"https://api.twelvedata.com/price?symbol={import_ticker}&apikey={import_api_key}"
+def get_stock_price():
+    url = f"https://api.twelvedata.com/price?symbol={ticker}&apikey={api_key}"
     response = requests.get(url).json()
     price = response['price'][:-3]
-    return price
+    print(f'{time.ctime(time.time())} : {price}')
 
-stock_price = get_stock_price(ticker, api_key)
-
-print(f'{time.time}:{stock_price}')
+starttime = time.time()
+while True:
+    get_stock_price()
+    time.sleep(5.0 - ((time.time() - starttime) % 5.0))
